12285a8e27448d904821ccd7bb1cdf890e8379a1
mbiti2 <louisembiti@gmail.com>
2025-03-13 07:44:10 +0100

    formatted code for correct indentation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{
-    fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, path::Path
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -16,19 +18,15 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    
 
     let mut build_response = |path: &str| {
         let status_line = "HTTP/1.1 200 OK";
         let contents = fs::read_to_string(path).unwrap();
         let length = contents.len();
-        
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-        
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
         stream.write_all(response.as_bytes()).unwrap();
-        
     };
     if request_line == "GET /login HTTP/1.1" {
         build_response("public/login.html");
